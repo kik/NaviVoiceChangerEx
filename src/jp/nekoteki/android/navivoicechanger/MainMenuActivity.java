1c9858002effa39e479a931d9ad56200f013e96f
HMML <hmml3939@gmail.com>
2013-07-20 18:00:17 +0900

    Add install list activity.

--- src/jp/nekoteki/android/navivoicechanger/MainMenuActivity.java
+++ src/jp/nekoteki/android/navivoicechanger/MainMenuActivity.java
@@ -2,7 +2,9 @@ package jp.nekoteki.android.navivoicechanger;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
 
 public class MainMenuActivity extends Activity {
 
@@ -10,6 +12,19 @@ public class MainMenuActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main_menu);
+		
+		
+		View.OnClickListener click_hdl_install = new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				Intent intent = new Intent(MainMenuActivity.this, InstallListActivity.class);
+				startActivity(intent);
+			}
+		};
+		
+		View btn_install = findViewById(R.id.btn_menu_install);
+		btn_install.setOnClickListener(click_hdl_install);
 	}
 
 	@Override
@@ -18,5 +33,7 @@ public class MainMenuActivity extends Activity {
 		getMenuInflater().inflate(R.menu.main_menu, menu);
 		return true;
 	}
+	
+	
 
 }

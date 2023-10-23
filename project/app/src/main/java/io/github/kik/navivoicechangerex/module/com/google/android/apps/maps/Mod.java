a2e9881e65bbf2fe3d147c9d46c5b9e9ed0bcfa8
kik <kik314@gmail.com>
2023-10-24 00:39:43 +0900

    add android project

---
+++ project/app/src/main/java/io/github/kik/navivoicechangerex/module/com/google/android/apps/maps/Mod.java
@@ -0,0 +1,12 @@
+package io.github.kik.navivoicechangerex.module.com.google.android.apps.maps;
+
+import de.robv.android.xposed.IXposedHookLoadPackage;
+import de.robv.android.xposed.XposedBridge;
+import de.robv.android.xposed.callbacks.XC_LoadPackage;
+
+public class Mod implements IXposedHookLoadPackage {
+    @Override
+    public void handleLoadPackage(XC_LoadPackage.LoadPackageParam lpparam) throws Throwable {
+        XposedBridge.log("Loaded app: " + lpparam.packageName);
+    }
+}
